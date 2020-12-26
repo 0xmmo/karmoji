cc120ed69986f3b30b867ef0e54efb7a1fe774ae
Paul Sherman <paul.sherman.88@gmail.com>
2020-12-25 16:26:13 -0800

    More self-review

--- app/handlers.js
+++ app/handlers.js
@@ -1,14 +1,15 @@
 const { markResponded } = require('./events');
 const { mentionedUsers } = require('./find');
 const { mentions } = require('./mentions');
-const { tacoMessage, negatacoMessage } = require('./messages');
+const { messages } = require('./messages');
 
 module.exports.handleMessage = function handleMessage(event) {
   const { text, channel, user, ts } = event;
   const users = mentionedUsers(text);
   if (users.length) {
-    tacoMessage(text, channel, ts, users, user);
-    negatacoMessage(text, channel, ts);
+    for (const message of messages) {
+      message(text, channel, ts, users, user);
+    }
     markResponded(event);
   }
 };
