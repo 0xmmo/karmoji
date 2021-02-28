ad9e0e0fab3b6a4a47ec02e67a308661dd7f9927
Paul Sherman <psherman@rescale.com>
2021-02-28 07:55:57 -0800

    Spam protection

---
+++ app/constants.ts
@@ -0,0 +1,26 @@
+const { emojify } = require('./utils');
+
+module.exports.TACO_EMOJIS = [
+  'ok_hand',
+  'fire',
+  'thumbsup',
+  'boom',
+  'tada',
+  'heart_eyes',
+  'star-struck',
+  'muscle',
+  'clap',
+  'raised_hands',
+  'rocket',
+  'dark_sunglasses',
+  'confetti_ball',
+  'gift',
+  'trophy',
+  'mega'
+];
+
+module.exports.NEGATACO_PHRASES = [
+  emojify('dang'),
+  emojify('#rude'),
+  emojify('really?')
+];
--- app/messages.js
+++ app/messages.js
@@ -1,7 +1,13 @@
-const { sendReaction, sendImage } = require('./send');
+const {
+  sendReaction,
+  sendImage,
+  sendMessage,
+  sendEphemeral
+} = require('./send');
 const { textIncludes } = require('./find');
-const { shuffle } = require('./utils');
+const { shuffle, emojify } = require('./utils');
 const { giveTaco } = require('./database');
+const { TACO_EMOJIS, NEGATACO_PHRASES } = require('./constants');
 
 function uniqueMentions(users, sender) {
   const mentionedUsers = new Set(users);
@@ -9,6 +15,111 @@ function uniqueMentions(users, sender) {
     mentionedUsers.delete(sender);
   }
   return Array.from(mentionedUsers);
+}
+
+function createTacoGiver(users) {
+  const givenTo = {};
+  for (const user of users) {
+    givenTo[user] = 1;
+  }
+  return {
+    spams: 0,
+    givenTo
+  };
+}
+
+const TACO_GIVERS = {};
+
+function logTacos(tacoState, users) {
+  let overTheLimit = false;
+  for (const user of users) {
+    if (user in tacoState.givenTo) {
+      tacoState.givenTo[user]++;
+      if (tacoState.givenTo[user] > 5) {
+        overTheLimit = true;
+      }
+    } else {
+      tacoState.givenTo[user] = 1;
+    }
+  }
+  if (overTheLimit) {
+    tacoState.spams++;
+  }
+  return overTheLimit;
+}
+
+function trackTacos(userFrom, users) {
+  let tacoState = TACO_GIVERS[userFrom];
+  let exceedsLimits = false;
+  if (tacoState) {
+    exceedsLimits = logTacos(tacoState, users);
+  } else {
+    tacoState = createTacoGiver(users);
+    TACO_GIVERS[userFrom] = tacoState;
+  }
+  // allow tacos if no user is over the limit
+  return exceedsLimits ? tacoState.spams : 0;
+}
+
+function resetTracking() {
+  for (const key in TACO_GIVERS) {
+    if (TACO_GIVERS.hasOwnProperty(key)) {
+      Reflect.deleteProperty(TACO_GIVERS, key);
+    }
+  }
+}
+
+// reset taco tracking every 24 hours
+const ONE_DAY = 24 * 60 * 60 * 1000;
+setInterval(resetTracking, ONE_DAY);
+
+function selectTacoEmojis(count) {
+  const emojis = shuffle(TACO_EMOJIS.slice());
+  return emojis.slice(0, count);
+}
+
+function spamReaction(channel, user, messageTimestamp, spams) {
+  switch (spams) {
+    case 1:
+      sendReaction(channel, messageTimestamp, emojify('hand'));
+      break;
+    case 2:
+      sendReaction(channel, messageTimestamp, emojify('?'));
+      break;
+    case 3:
+      sendReaction(channel, messageTimestamp, emojify('thinking_face'));
+      break;
+    case 4:
+      sendReaction(channel, messageTimestamp, emojify('??'));
+      break;
+    case 5:
+      sendReaction(channel, messageTimestamp, emojify('um'));
+      break;
+    case 6:
+      sendMessage(channel, 'BTW, I stop counting at 5 tacos a day per person');
+      break;
+    case 7:
+      sendImage(
+        channel,
+        'https://media0.giphy.com/media/zCfeMBYXjY9yM/giphy.gif'
+      );
+      break;
+    case 8:
+      sendEphemeral(
+        channel,
+        user,
+        'I have created a new emoji just for this situation, though. Would you like to see it?'
+      );
+      break;
+    case 9:
+      sendEphemeral(channel, user, ':taco-jon:');
+      break;
+    case 10:
+      sendEphemeral(channel, user, 'Pretty cool, right?');
+      break;
+    default:
+      sendReaction(channel, messageTimestamp, 'taco-jon');
+  }
 }
 
 module.exports.messages = [
@@ -19,32 +130,20 @@ module.exports.messages = [
     }
 
     const mentions = uniqueMentions(users, userFrom);
+
+    const spams = trackTacos(userFrom, users);
+    if (spams > 0) {
+      spamReaction(channel, userFrom, messageTimestamp, spams);
+      return;
+    }
+
     for (const user of mentions) {
       giveTaco(channel, userFrom, user);
     }
 
-    const emojis = shuffle([
-      'ok_hand',
-      'fire',
-      'thumbsup',
-      'boom',
-      'tada',
-      'heart_eyes',
-      'star-struck',
-      'muscle',
-      'clap',
-      'raised_hands',
-      'rocket',
-      'dark_sunglasses',
-      'confetti_ball',
-      'gift',
-      'trophy',
-      'mega'
-    ]);
     const count = mentions.length;
-
-    for (let i = 0; i < count; i++) {
-      const emoji = emojis[i % emojis.length];
+    const emojis = selectTacoEmojis(count);
+    for (const emoji of emojis) {
       sendReaction(channel, messageTimestamp, emoji);
     }
 
@@ -55,9 +154,11 @@ module.exports.messages = [
   // negatacos
   (text, channel, messageTimestamp) => {
     if (textIncludes(text, [/:negataco:/g])) {
-      const emojis = ['notsureif', 'coneofshame', 'wat', 'wutlol'];
+      const emojis = NEGATACO_PHRASES;
       const emoji = emojis[Math.floor(Math.random() * emojis.length)];
-      sendReaction(channel, messageTimestamp, emoji);
+      for (const char of emoji) {
+        sendReaction(channel, messageTimestamp, char);
+      }
     }
   }
 ];
